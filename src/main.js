7991dc5b66bcf98aed2eb91051ee6c80d7d69eea
Lydia Solomon <lydiasolomon97@gmail.com>
2025-02-06 13:05:15 +0300

    feat: add a sphere element
    Signed-off-by: Lydia Solomon <lydiasolomon97@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import * as THREE from "three";
+import "/src/style.css";
+import { OrbitControls } from "three/examples/jsm/Addons.js";
 
 const scene = new THREE.Scene();
 
@@ -10,10 +12,50 @@ const material = new THREE.MeshStandardMaterial({
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
-const camera = new THREE.PerspectiveCamera(45, 800, 600);
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+const light = new THREE.PointLight(0xffffff, 70, 100, 1.7);
+light.position.set(0, 10, 10);
+scene.add(light);
+
+const camera = new THREE.PerspectiveCamera(
+  45,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.z = 20;
 scene.add(camera);
 
 const canvas = document.querySelector(".webgl");
 const renderer = new THREE.WebGLRenderer({ canvas });
-renderer.setSize(800, 600);
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(2);
 renderer.render(scene, camera);
+
+window.addEventListener("resize", () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(sizes.width, sizes.height);
+});
+
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+controls.enableDamping = false;
+controls.enableZoom = false;
+controls.autoRotate = true;
+controls.autoRotateSpeed = 5;
+
+const loop = () => {
+  controls.update();
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(loop);
+};
+
+loop();
